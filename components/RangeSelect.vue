82deebf5268e2777733b40739893d5c30d48caf6
Po YZ <poyizhi@gmail.com>
2024-04-05 17:59:46 +0800

    chore: code formatting

--- components/RangeSelect.vue
+++ components/RangeSelect.vue
@@ -1,21 +1,33 @@
 <script setup lang="ts">
-const props = withDefaults(defineProps<{
-    label: string,
-    name: string,
-    min: number,
-    max: number,
-    step: number
-    disabled?: boolean,
-    defaultValue: number,
-    handleOnChange: (e: Event) => void
-}>(), {
-    disabled: false
-})
+  const props = withDefaults(
+    defineProps<{
+      label: string;
+      name: string;
+      min: number;
+      max: number;
+      step: number;
+      disabled?: boolean;
+      defaultValue: number;
+      handleOnChange: (e: Event) => void;
+    }>(),
+    {
+      disabled: false,
+    }
+  );
 </script>
 <template>
-    <label :for="name">
-        <div>{{ label }}</div>
-        <input type="range" :name="name" :id="name" :value="defaultValue" :min="min" :max="max" :step="step"
-            :disabled="disabled" @change="handleOnChange">
-    </label>
+  <label :for="name">
+    <div>{{ label }}: [{{ defaultValue }}]</div>
+    <input
+      type="range"
+      :name="name"
+      :id="name"
+      :value="defaultValue"
+      :min="min"
+      :max="max"
+      :step="step"
+      :disabled="disabled"
+      @change="handleOnChange"
+    />
+  </label>
 </template>
